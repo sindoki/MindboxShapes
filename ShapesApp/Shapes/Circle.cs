135d8846f4a6841d0134d569773c26bc4bb08ef1
Eugene Melnikov <apinae1@gmail.com>
2019-07-21 19:23:59 +0300

    Project init

---
+++ ShapesApp/Shapes/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public float Radius
+        {
+            get;
+            private set;
+        }
+
+        public Circle(float radius)
+        {
+            Radius = radius;
+        }
+
+        public override float GetSquare()
+        {
+            return (float)(Math.PI * Radius * Radius);
+        }
+    }
+}
---
+++ ShapesApp/Shapes/Shape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    /// <summary>
+    /// Базовый тип геометрической фигуры
+    /// </summary>
+    public abstract class Shape
+    {
+        /// <summary>
+        /// Площадь фигуры
+        /// </summary>
+        public abstract float GetSquare();
+    }
+}
